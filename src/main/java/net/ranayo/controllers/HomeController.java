8ab1c71c050f278b074c62fdefa169aec8e5258b
pjmDevelop <wjdals6445580@gmail.com>
2019-02-24 20:39:25 +0900

    Home Controller 추가

--- src/main/java/net/ranayo/controllers/HomeController.java
+++ src/main/java/net/ranayo/controllers/HomeController.java
@@ -1,6 +1,5 @@
 package net.ranayo.controllers;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
