7848ebe8339df4eae96cebaceedb890500002355
pjmDevelop <wjdals6445580@gmail.com>
2019-02-25 22:25:56 +0900

    DB Connect and Entity create (first)

--- src/main/java/net/ranayo/model/user.java
+++ src/main/java/net/ranayo/model/user.java
@@ -1,5 +1,78 @@
 package net.ranayo.model;
 
-public class user {
+import java.sql.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class user {
+	@Id
+	private String usEmail; // 아이디 겸 이메일
+	private String usPasswd; // 비밀번호
+	private String usNickname; // 닉네임
+	private Date usJoindt; // 가입날자
+	private String usJoinip; // 가입할때 ip
+	private Date usUpdt; // 아이디 최종 수정날
+	private String usUpip; // 아이디 최종 수정 아이피
+	private String usState; // 가입상태 ( 탈퇴, 신고차단, 정상 )
+	private int usAuth; // 해당 유저 권한
+	
+	
+	public String getUsEmail() {
+		return usEmail;
+	}
+	public void setUsEmail(String usEmail) {
+		this.usEmail = usEmail;
+	}
+	public String getUsPasswd() {
+		return usPasswd;
+	}
+	public void setUsPasswd(String usPasswd) {
+		this.usPasswd = usPasswd;
+	}
+	public String getUsNickname() {
+		return usNickname;
+	}
+	public void setUsNickname(String usNickname) {
+		this.usNickname = usNickname;
+	}
+	public Date getUsJoindt() {
+		return usJoindt;
+	}
+	public void setUsJoindt(Date usJoindt) {
+		this.usJoindt = usJoindt;
+	}
+	public String getUsJoinip() {
+		return usJoinip;
+	}
+	public void setUsJoinip(String usJoinip) {
+		this.usJoinip = usJoinip;
+	}
+	public Date getUsUpdt() {
+		return usUpdt;
+	}
+	public void setUsUpdt(Date usUpdt) {
+		this.usUpdt = usUpdt;
+	}
+	public String getUsUpip() {
+		return usUpip;
+	}
+	public void setUsUpip(String usUpip) {
+		this.usUpip = usUpip;
+	}
+	public String getUsState() {
+		return usState;
+	}
+	public void setUsState(String usState) {
+		this.usState = usState;
+	}
+	public int getUsAuth() {
+		return usAuth;
+	}
+	public void setUsAuth(int usAuth) {
+		this.usAuth = usAuth;
+	}
+
+	
 }
---
+++ src/main/java/net/ranayo/repository/userRepository.java
@@ -0,0 +1,11 @@
+package net.ranayo.repository;
+
+/**
+ * @Name : Park Jung Min
+ * @Date : 2019. 02. 25.
+ * @Description : User Repository
+ */
+
+public interface userRepository {
+
+}
